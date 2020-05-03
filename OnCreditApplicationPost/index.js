381357442e227fb9476bcf7007fbd15fbfe1a3e9
Jose Chavez <jose.chavez@slalom.com>
2020-05-03 12:05:29 -0500

    Sending email via SendGrid

--- OnCreditApplicationPost/index.js
+++ OnCreditApplicationPost/index.js
@@ -1,16 +1,59 @@
+const fieldMappings = {
+    firstName: 'First Name',
+    lastName: 'Last Name',
+    addressLine1: 'Address Line 1',
+    addressLine2: 'Address Line 2',
+    city: 'City',
+    state: 'State',
+    zipCode: 'Zip Code',
+    employerName: 'Employer Name',
+    yearsEmployed: 'Years Employed',
+    yearlySalary: 'Yearly Salary',
+    email: 'E-Mail Address',
+    creditCheckAgreement: 'Agrees to Credit Check?',
+};
+
+const createEmailBody = (userData) => {
+    var emailBody = '<p>A credit application has been submitted with the following information:</p><br/>';
+
+    emailBody += '<table>';
+
+    for (const property in userData) {
+        emailBody += `<tr><td>${fieldMappings[property]}</td><td>${userData[property]}</td></tr>`
+    }
+
+    emailBody += '</table>';
+
+    return emailBody;
+};
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
+    var userData = req.body;
+
+    var emailBody = createEmailBody(userData);
+
+    var message = {
+        personalizations: [
+            {
+                to: [ { email: "jose.e.chavez@outlook.com" } ]
+            } 
+        ],
+        from: { email: "jose.e.chavez@gmail.com" },
+        subject: "Dover Credit Application POC",
+        content: [{
+            type: 'text/html',
+            value: emailBody,
+        }]
+    };
+
+    context.res = {
+        status: 200,
+        body: "Credit Application mailed successfully",
     }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
+
+    context.done(null, message);
+   
+    return message;
 };
