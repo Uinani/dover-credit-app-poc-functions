7dcc570f88e2a58b85d925d286c1c1cce1fca309
Jose <jose.e.chavez@gmail.com>
2020-07-11 00:41:40 -0500

    Using Azure Storage SDK

--- OnCreditApplicationPost/index.js
+++ OnCreditApplicationPost/index.js
@@ -1,25 +1,63 @@
-const fs = require('fs');
+const {
+  Aborter,
+  StorageURL,
+  ServiceURL,
+  ShareURL,
+  DirectoryURL,
+  FileURL,
+  SharedKeyCredential,
+} = require("@azure/storage-file");
 const Mustache = require('mustache');
 
-const createEmailBody = (userData) => {
-  var path = __dirname + '//email-template.html';
-  fs.readFile(path, 'utf8', function (err, template) {
-    if (err) {
-      context.log.error(err);
-      context.done(err);
-    }
-
-    var rendered = Mustache.render(template, userData);
-
-    return [rendered, userData.overrideEmail];
+// A helper method used to read a Node.js readable stream into string
+async function streamToString(readableStream) {
+  return new Promise((resolve, reject) => {
+    const chunks = [];
+    readableStream.on("data", (data) => {
+      chunks.push(data.toString());
+    });
+    readableStream.on("end", () => {
+      resolve(chunks.join(""));
+    });
+    readableStream.on("error", reject);
   });
+}
+
+const createEmailBody = async (userData) => {
+  const account = process.env.AzureStorageAccountName;
+  const accountKey = process.env.AzureStorageAccessKey;
+
+  const sharedKeyCredential = new SharedKeyCredential(account, accountKey);
+  const pipeline = StorageURL.newPipeline(sharedKeyCredential);
+  const serviceURL = new ServiceURL(
+    // When using AnonymousCredential, following url should include a valid SAS
+    `https://${account}.file.core.windows.net`,
+    pipeline
+  );
+
+  const shareName = 'dover-credit-fs-public';
+  const shareURL = ShareURL.fromServiceURL(serviceURL, shareName);
+
+  const directoryName = 'emailTemplates';
+  const directoryURL = DirectoryURL.fromShareURL(shareURL, directoryName);
+  
+  const fileName = 'email-template.html';
+  const fileURL = FileURL.fromDirectoryURL(directoryURL, fileName);
+  
+  const downloadFileResponse = await fileURL.download(Aborter.none, 0);
+
+  const template = await streamToString(downloadFileResponse.readableStreamBody);
+
+  var rendered = Mustache.render(template, userData);
+
+  return [rendered, userData.overrideEmail];
 };
 
 module.exports = async function (context, req) {
   context.log('JavaScript HTTP trigger function processed a request.');
 
   return createEmailBody(req.body)
-    .then((emailBody, overrideEmail) => {
+    .then(([emailBody, overrideEmail]) => {
       var email = overrideEmail ? overrideEmail : "greenawaybb@gmail.com";
       
       return {
